d186f6d5f2bf6c30c661c9b221b3b73c3f173d64
Johannes <info@johannesmutter.de>
2024-03-23 17:19:46 +0100

    initial commit

---
+++ src/lib/Component.svelte
@@ -0,0 +1,24 @@
+<script>
+	let {
+		rows = 3
+	} = $props();
+  const title_font = $derived( rows <= 3 ? 'text-system' : 'text-intro' );
+</script>
+
+<p class='{title_font} title bold'>Dynamic class added at start: This text will be NOT bold initially</p>
+<p class='title bold {title_font} '>Dynamic class added at ed: This text will be bold initially</p>
+
+
+<style>
+	.bold {
+		font-weight: 700 !important;
+	}
+	.text-system {
+		font-size: 1rem;
+		font-weight: 400;
+	}
+	.text-intro {
+		font-size: 3rem;
+		font-weight: 400;
+	}
+</style>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,4 @@
+<script>
+  import Component from "$lib/Component.svelte";
+</script>
+<Component rows={3} />
